45893bef10990c2efa13d44ca3c0b5008ad11734
RemiSaurel <r.saurel5@gmail.com>
2023-10-06 16:43:58 +0200

    add: first commit

---
+++ src/test/java/fr/miage/consommateur/ConsommateurApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.miage.consommateur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsommateurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

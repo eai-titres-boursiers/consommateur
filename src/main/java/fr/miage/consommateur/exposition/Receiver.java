6962a631d58e9e4ef63c49991110d0bb58bf1332
RemiSaurel <r.saurel5@gmail.com>
2023-11-01 09:58:30 +0100

    add: logger

--- src/main/java/fr/miage/consommateur/exposition/Receiver.java
+++ src/main/java/fr/miage/consommateur/exposition/Receiver.java
@@ -1,10 +1,15 @@
 package fr.miage.consommateur.exposition;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Receiver {
+    private static final Logger logger = LoggerFactory.getLogger(Receiver.class);
+
     public void onMessageReceived(String message) {
-        System.out.println("Received [" + message + "]");
+        logger.info("Received [" + message + "]");
     }
+
 }
